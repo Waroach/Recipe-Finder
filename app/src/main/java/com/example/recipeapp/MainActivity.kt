e115414e2439f086af2c547c9a421699dea3bd18
waroach <brent.rankin@gmail.com>
2024-07-23 16:28:19 -0600

    Random Recipe is now working

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -3,20 +3,17 @@ package com.example.recipeapp
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-// need to import this manually maybe?
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.recipeapp.ui.SearchBarUI
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.e("onCreate", "Did i run?")
+        Log.e("onCreate", "Did I run?")
         super.onCreate(savedInstanceState)
         setContent {
             RecipeAppTheme {
--- app/src/main/java/com/example/recipeapp/ui/RecipeScreen.kt
+++ app/src/main/java/com/example/recipeapp/ui/RecipeScreen.kt
@@ -7,9 +7,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.recipeapp.ui.SampleData
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -105,77 +106,82 @@ fun RandomRecipeButton(coroutineScope: CoroutineScope, onRandomRecipeClick: susp
  * Placeholder function for fetching a random recipe (to be implemented later).
  */
 suspend fun fetchRandomRecipe(): Recipe? {
-    val retrofit = Retrofit.Builder()
-        .baseUrl("https://www.themealdb.com/api/json/v1/1/") // Base URL of the API
-        .addConverterFactory(GsonConverterFactory.create()) // Use Gson for JSON parsing
-        .build()
+    return withContext(Dispatchers.IO) {
+        Log.d("fetchRandomRecipe", "Did I fetch?")
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.themealdb.com/api/json/v1/1/") // Base URL of the API
+            .addConverterFactory(GsonConverterFactory.create()) // Use Gson for JSON parsing
+            .build()
 
-    val mealApiService = retrofit.create(MealApiService::class.java)
+        val mealApiService = retrofit.create(MealApiService::class.java)
 
-    return try {
-        val response = mealApiService.getRandomRecipe().execute()
-        Log.d("RecipeApp", "API Response: ${response.code()}") // Log response code
-        if (response.isSuccessful) {
-            val recipeResponse = response.body()
-            val meal = recipeResponse?.meals?.firstOrNull()
-            if (meal != null) {
-                Log.d("RecipeApp", "Fetched Recipe Name: $meal.strMeal") // Log fetched recipe
-                Recipe(id = meal.idMeal,
-                    name = meal.strMeal,
-                    instructions = meal.strInstructions,
-                    mealThumb = meal.strMealThumb,
-                    youtube = meal.strYoutube,
-                    ingredient1 = meal.strIngredient1,
-                    ingredient2 = meal.strIngredient2,
-                    ingredient3 = meal.strIngredient3,
-                    ingredient4 = meal.strIngredient4,
-                    ingredient5 = meal.strIngredient5,
-                    ingredient6 = meal.strIngredient6,
-                    ingredient7 = meal.strIngredient7,
-                    ingredient8 = meal.strIngredient8,
-                    ingredient9 = meal.strIngredient9,
-                    ingredient10 = meal.strIngredient10,
-                    ingredient11 = meal.strIngredient11,
-                    ingredient12 = meal.strIngredient12,
-                    ingredient13 = meal.strIngredient13,
-                    ingredient14 = meal.strIngredient14,
-                    ingredient15 = meal.strIngredient15,
-                    ingredient16 = meal.strIngredient16,
-                    ingredient17 = meal.strIngredient17,
-                    ingredient18 = meal.strIngredient18,
-                    ingredient19 = meal.strIngredient19,
-                    ingredient20 = meal.strIngredient20,
-                    measure1 = meal.strMeasure1,
-                    measure2 = meal.strMeasure2,
-                    measure3 = meal.strMeasure3,
-                    measure4 = meal.strMeasure4,
-                    measure5 = meal.strMeasure5,
-                    measure6 = meal.strMeasure6,
-                    measure7 = meal.strMeasure7,
-                    measure8 = meal.strMeasure8,
-                    measure9 = meal.strMeasure9,
-                    measure10 = meal.strMeasure10,
-                    measure11 = meal.strMeasure11,
-                    measure12 = meal.strMeasure12,
-                    measure13 = meal.strMeasure13,
-                    measure14 = meal.strMeasure14,
-                    measure15 = meal.strMeasure15,
-                    measure16 = meal.strMeasure16,
-                    measure17 = meal.strMeasure17,
-                    measure18 = meal.strMeasure18,
-                    measure19 = meal.strMeasure19,
-                    measure20 = meal.strMeasure20,
-                    source = meal.strSource,
-                    // Map other fields as needed
-                )
+        try {
+            val response = mealApiService.getRandomRecipe().execute()
+            Log.d("RecipeApp", "API Response: ${response.code()}") // Log response code
+            if (response.isSuccessful) {
+                val recipeResponse = response.body()
+                val meal = recipeResponse?.meals?.firstOrNull()
+                if (meal != null) {
+                    Log.d("RecipeApp", "Fetched Recipe Name: $meal.strMeal") // Log fetched recipe
+                    Recipe(id = meal.idMeal,
+                        name = meal.strMeal,
+                        instructions = meal.strInstructions,
+                        mealThumb = meal.strMealThumb,
+                        youtube = meal.strYoutube,
+                        ingredient1 = meal.strIngredient1,
+                        ingredient2 = meal.strIngredient2,
+                        ingredient3 = meal.strIngredient3,
+                        ingredient4 = meal.strIngredient4,
+                        ingredient5 = meal.strIngredient5,
+                        ingredient6 = meal.strIngredient6,
+                        ingredient7 = meal.strIngredient7,
+                        ingredient8 = meal.strIngredient8,
+                        ingredient9 = meal.strIngredient9,
+                        ingredient10 = meal.strIngredient10,
+                        ingredient11 = meal.strIngredient11,
+                        ingredient12 = meal.strIngredient12,
+                        ingredient13 = meal.strIngredient13,
+                        ingredient14 = meal.strIngredient14,
+                        ingredient15 = meal.strIngredient15,
+                        ingredient16 = meal.strIngredient16,
+                        ingredient17 = meal.strIngredient17,
+                        ingredient18 = meal.strIngredient18,
+                        ingredient19 = meal.strIngredient19,
+                        ingredient20 = meal.strIngredient20,
+                        measure1 = meal.strMeasure1,
+                        measure2 = meal.strMeasure2,
+                        measure3 = meal.strMeasure3,
+                        measure4 = meal.strMeasure4,
+                        measure5 = meal.strMeasure5,
+                        measure6 = meal.strMeasure6,
+                        measure7 = meal.strMeasure7,
+                        measure8 = meal.strMeasure8,
+                        measure9 = meal.strMeasure9,
+                        measure10 = meal.strMeasure10,
+                        measure11 = meal.strMeasure11,
+                        measure12 = meal.strMeasure12,
+                        measure13 = meal.strMeasure13,
+                        measure14 = meal.strMeasure14,
+                        measure15 = meal.strMeasure15,
+                        measure16 = meal.strMeasure16,
+                        measure17 = meal.strMeasure17,
+                        measure18 = meal.strMeasure18,
+                        measure19 = meal.strMeasure19,
+                        measure20 = meal.strMeasure20,
+                        source = meal.strSource,
+                        // Map other fields as needed
+                    )
+                } else {
+                    null
+                }
             } else {
+                Log.e("RecipeApp", "API Error: ${response} or ${response.code()}") // Log error response code
                 null
             }
-        } else {
+        } catch (e: Exception) {
+            Log.e("RecipeApp", "API Exception: ${e}") // Log error message
             null
         }
-    } catch (e: Exception) {
-        null
     }
 }
 
